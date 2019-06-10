e8c85248237a94d6590cf8d0748268f5b0981653
Eric Frick <eric.frick@icloud.com>
2019-06-10 16:09:59 -0400

    Added signup functionality

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,16 +1,45 @@
 import app from 'firebase/app'
+import auth from 'firebase/auth'
 
 import config from './firebase'
-import FirebaseContext from './context'
+import FirebaseContext, { withFirebase } from './context'
 
 class Firebase {
 
     constructor() {
         app.initializeApp(config)
+
+        this.auth = app.auth()
+
+        this.doCreateUserWithEmailAndPassword = this.doCreateUserWithEmailAndPassword.bind(this)
+        this.doSignInWithEmailAndPassword = this.doSignInWithEmailAndPassword.bind(this)
+        this.doSignOut = this.doSignOut.bind(this)
+        this.doPasswordReset = this.doPasswordReset.bind(this)
+        this.doPasswordUpdate = this.doPasswordUpdate.bind(this)
+    }
+
+    doCreateUserWithEmailAndPassword(email, password) {
+        return this.auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    doSignInWithEmailAndPassword(email, password) {
+        return this.auth.signInWithEmailAndPassword(email, password)
+    }
+
+    doSignOut() {
+        return this.auth.signOut()
+    }
+
+    doPasswordReset(email) {
+        return this.auth.sendPasswordResetEmail()
+    }
+
+    doPasswordUpdate(password) {
+        return this.auth.currentUser.updatePassword(password)
     }
 
 }
 
 export default Firebase
 
-export { FirebaseContext }
+export { FirebaseContext, withFirebase }
---
+++ src/components/SignUp/index.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import { withFirebase } from '../Firebase'
+import * as ROUTES from '../../constants/routes'
+
+const SignUpPage = () => (
+    <div>
+        <h1>Sign Up</h1>
+        <SignupForm />
+    </div>
+)
+
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    error: null
+}
+
+class SignupFormBase extends Component {
+    constructor(props) {
+        super(props)
+
+        this.onSubmit = this.onSubmit.bind(this)
+        this.onChange = this.onChange.bind(this)
+
+        this.state = { ...INITIAL_STATE }
+    }
+
+    onSubmit(evt) {
+        const {username, email, password } = this.state
+
+        this.props.firebase
+            .doCreateUserWithEmailAndPassword(email, password)
+            .then(user => {
+                this.setState({ ...INITIAL_STATE })
+            })
+            .catch(error => {
+                this.setState({ error })
+            })
+
+        evt.preventDefault()
+    }
+
+    onChange(evt) {
+        this.setState({ [evt.target.name]: evt.target.value })
+    }
+
+    render() {
+        const {
+            username,
+            email,
+            password,
+            passwordConfirm,
+            error
+        } = this.state
+
+        const isInvalid = password !== passwordConfirm || password === '' || email === '' || username === ''
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input name="username" value={username} onChange={this.onChange} type="text" placeholder="Full Name" />
+                <input name="email"    value={email}    onChange={this.onChange} type="text" placeholder="Email Address" />
+                <input name="password" value={password} onChange={this.onChange} type="password" placeholder="Password" />
+                <input name="passwordConfirm" value={passwordConfirm} onChange={this.onChange} type="password" placeholder="Confirm Password" />
+
+                <button type="submit" disabled={isInvalid}>Sign Up</button>
+
+                { error && <p>{error.message}</p> }
+            </form>
+        )
+    }
+}
+
+const SignupLink = () => (
+    <p>
+        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </p>
+)
+
+const SignupForm = withFirebase(SignupFormBase)
+
+export default SignUpPage
+
+export { SignupForm, SignupLink }
