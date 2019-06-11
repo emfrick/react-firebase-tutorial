19fa8100fe2551daf0f907e1842eb9f82b71e100
Eric Frick <eric.frick@icloud.com>
2019-06-11 16:36:06 -0400

    Added Firebase database connections

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app'
 import auth from 'firebase/auth'
+import 'firebase/database'
 
 import config from './firebase'
 import FirebaseContext, { withFirebase } from './context'
@@ -10,13 +11,19 @@ class Firebase {
         app.initializeApp(config)
 
         this.auth = app.auth()
+        this.db = app.database()
 
         this.doCreateUserWithEmailAndPassword = this.doCreateUserWithEmailAndPassword.bind(this)
         this.doSignInWithEmailAndPassword = this.doSignInWithEmailAndPassword.bind(this)
         this.doSignOut = this.doSignOut.bind(this)
         this.doPasswordReset = this.doPasswordReset.bind(this)
         this.doPasswordUpdate = this.doPasswordUpdate.bind(this)
+
+        this.user = this.user.bind(this)
+        this.users = this.users.bind(this)
     }
+
+    /*========== AUTH API ==========*/
 
     doCreateUserWithEmailAndPassword(email, password) {
         return this.auth.createUserWithEmailAndPassword(email, password)
@@ -38,6 +45,14 @@ class Firebase {
         return this.auth.currentUser.updatePassword(password)
     }
 
+    /*========== USER API ==========*/
+    user(uid) {
+        return this.db.ref(`users/${uid}`)
+    }
+
+    users() {
+        return this.db.ref('users')
+    }
 }
 
 export default Firebase
