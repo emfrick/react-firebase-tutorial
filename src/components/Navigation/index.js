9790204b3755385cc7084000f1cb99bb84b9027f
Eric Frick <eric.frick@icloud.com>
2019-06-11 10:53:42 -0400

    Added session handling in app component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,15 +4,13 @@ import { Link } from 'react-router-dom'
 import SignoutButton from '../SignOut'
 import * as ROUTES from '../../constants/routes'
 
-const Navigation = () => (
+const Navigation = ({ user }) => (
+    <div>{ user? <NavigationAuth /> : <NavigationNonAuth /> }</div>
+)
+
+const NavigationAuth = () => (
     <div>
         <ul>
-            <li>
-                <Link to={ ROUTES.SIGN_IN }>Sign In</Link>
-            </li>
-            <li>
-                <Link to={ ROUTES.SIGN_UP }>Sign Up</Link>
-            </li>
             <li>
                 <Link to={ ROUTES.LANDING }>Landing</Link>
             </li>
@@ -23,13 +21,21 @@ const Navigation = () => (
                 <Link to={ ROUTES.ACCOUNT }>Account</Link>
             </li>
             <li>
-                <Link to={ ROUTES.ADMIN }>Admin</Link>
-            </li>
-            <li>
                 <SignoutButton />
             </li>
         </ul>
     </div>
+)
+
+const NavigationNonAuth = () => (
+    <ul>
+        <li>
+            <Link to={ ROUTES.LANDING }>Landing</Link>
+        </li>
+        <li>
+            <Link to={ ROUTES.SIGN_IN }>Sign In</Link>
+        </li>
+    </ul>
 )
 
 export default Navigation
